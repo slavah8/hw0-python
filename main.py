04754e79d7e7ae225dc0dd985cd1a60d0eb77cec
slavah8 <vhq5091@psu.edu>
2020-08-29 21:58:19 +0000

    changed the float

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 celsius=float(input("Enter temperature in celsius: "))
 fahrenheit=(celsius*1.8) +32
-print("%.2f° in Celsius is equivalent to %0.2f° Fahrenheit." %(celsius, fahrenheit))
+print("%.1f° in Celsius is equivalent to %0.1f° Fahrenheit." %(celsius, fahrenheit))
